1a09994aeb0e136e6dc12d89d46309727fe2efa9
Jasonwang911 <396689144@qq.com>
2018-12-19 16:18:38 +0800

    Feat(pm2):配置pm2配置文件

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 ## 装修门户koa后台
 
+
+### 本地启动   需要本地开启 redis mysql  
+```
+nondemon start
+```
+
+## centos启动  安装pm2  ecosystem.config.js为pm2配置文件
+```
+pm2 start ecosystem.config.js
+// 或者
+pm2 start --name "name" -- run centos
+```
+
+查看监控日志 ： pm2 monit   
+
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,29 @@
 module.exports = {
   apps : [{
     name: 'QRserver',
-    script: 'bin/www',
-    exec_interpreter: "babel-node",
+    script: './bin/www',
+    cwd: "./",
+    interpreter: "babel-node",
     node_args: "--harmony",
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
     instances: 1,
     autorestart: true,
-    watch: true,
+    watch: false,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
       NODE_ENV: 'production'
     }
   }],
 
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  // deploy : {
+  //   production : {
+  //     user : 'node',
+  //     host : '212.83.163.1',
+  //     ref  : 'origin/master',
+  //     repo : 'git@github.com:repo.git',
+  //     path : '/var/www/production',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  //   }
+  // }
 };
