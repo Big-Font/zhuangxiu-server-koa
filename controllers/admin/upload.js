f3b8e69cfd4f10033c5ad46b28e0e2cc44c36867
Jasonwang911 <396689144@qq.com>
2019-01-24 14:54:43 +0800

    Feat(upload):图片上传bug修改

--- controllers/admin/upload.js
+++ controllers/admin/upload.js
@@ -8,8 +8,8 @@ class UploadControllers {
         const { fields,files } = ctx.request;
         let file = JSON.parse(JSON.stringify(files)).file; // 获取上传文件
         let reader = fs.createReadStream(file.path);
-        let filePath = path.join(__dirname.replace('/controllers/admin', ''), 'public/images/') + `${file.name}`;
-        let newPath = `${config[process.env.NODE_ENV].address}/images/${file.name}`;
+        let filePath = path.join(__dirname.replace('/controllers/admin', ''), 'public/images/ueditor/') + `${file.name}`;
+        let newPath = `${config[process.env.NODE_ENV].address}/images/ueditor/${file.name}`;
         // 创建可写流
         const upStream = fs.createWriteStream(filePath);
         // 可读流通过管道写入可写流
