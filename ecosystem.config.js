63103b020bfb649b33bbe5845103e5f284a37f67
Jasonwang911 <396689144@qq.com>
2019-02-11 10:30:21 +0800

    Feat(pm2):pm2配置文件修改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,29 +1,48 @@
-module.exports = {
-  apps : [{
-    name: 'QRserver',
-    script: './bin/www',
-    cwd: "./",
-    interpreter: "babel-node",
-    node_args: "--harmony",
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: '',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production'
-    }
-  }],
+// module.exports = {
+//   apps : [
+//     {
+//       name: 'QRserver',
+//       script: './bin/www',
+//       cwd: "./",
+//       interpreter: "babel-node",
+//       node_args: "--harmony",
+//       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+//       args: '',
+//       instances: 1,
+//       //watch: false,
+//       //autorestart: true,
+//       watch: [  // 监控变化的目录，一旦变化，自动重启
+//      	 "bin",
+//      	 "routes"
+//       ],
+//       ignore_watch : ["./node_modules", "./public/*"],
+//       max_memory_restart: '1G',
+//       env: {
+//         NODE_ENV: 'production'
+//       },
+//       env_production: {
+//         NODE_ENV: "production",
+//       }
+//     }
+//   ],
+// };
 
-  // deploy : {
-  //   production : {
-  //     user : 'node',
-  //     host : '212.83.163.1',
-  //     ref  : 'origin/master',
-  //     repo : 'git@github.com:repo.git',
-  //     path : '/var/www/production',
-  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-  //   }
-  // }
-};
+module.exports = {
+  "apps" : [
+    {
+      name: "QRserver2222",
+      script: "./bin/www",
+      cwd: "./",
+      interpreter: "babel-node",
+      node_args: "--harmony",
+      watch: false,
+      ignore_watch: ["*"],
+      env: {
+        NODE_ENV: 'production'
+      },
+      env_production: {
+        NODE_ENV: "production",
+      }
+    }
+  ]
+}
